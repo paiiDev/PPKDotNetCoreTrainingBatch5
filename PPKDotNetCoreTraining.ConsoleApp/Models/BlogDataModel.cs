84de98df552135621bf2558ac4258d71cd47e778
Paii <pyaepaikyaw@gmail.com>
2025-12-07 19:16:00 +0630

    feat: adding Models(DTO)

---
+++ PPKDotNetCoreTraining.ConsoleApp/Models/BlogDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPKDotNetCoreTraining.ConsoleApp.Models
+{
+    internal class BlogDataModel
+    {
+        public int blogId { get; set; }
+        public string blogTitle { get; set; }
+        public string blogAuthor { get; set; }
+        public string blogContent { get; set; }
+    }   
+}
