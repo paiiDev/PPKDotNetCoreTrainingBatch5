e2af505c27af3c36672b15debd6328c3e7c423b4
Paii <pyaepaikyaw@gmail.com>
2025-12-19 14:38:11 +0630

    feat: BirdsMinimalApi and AgricultureRestApi

---
+++ BurmeseAgriculture.RestApi/Controllers/AgricultureController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace BurmeseAgriculture.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgricultureController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAgricultureInfo()
+        {
+            string filePath = "Data/BurmeseAgriculture.json";
+            var jsonStr = System.IO.File.ReadAllText(filePath);
+            var result = JsonConvert.DeserializeObject<List<AgricultureModel>>(jsonStr);
+
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAgricultureById(string id)
+        {
+            string filePath = "Data/BurmeseAgriculture.json";
+            var jsonStr = System.IO.File.ReadAllText(filePath);
+            var result = JsonConvert.DeserializeObject<List<AgricultureModel>>(jsonStr);
+            var item = result.FirstOrDefault(x => x.Id == id);
+
+            return Ok(item);
+        }
+
+
+
+
+        public class AgricultureModel
+        {
+            public string Id { get; set; }
+            public string Title { get; set; }
+            public DateTime Date { get; set; }
+            public string Author { get; set; }
+            public string Content { get; set; }
+        }
+
+    }
+}
---
+++ DotNetBatch5.BirdsMinimalApi/Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Newtonsoft.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/birds", () =>
+{
+    string folderPath = "Data/Birds/Birds.json";
+    var jsonStr =  File.ReadAllText(folderPath);
+    var reult =  JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr);
+    return Results.Ok(reult.Tbl_Bird);
+})
+.WithName("GetBirds")
+.WithOpenApi();
+
+
+app.MapGet("/birds/{id}", (int id) =>
+{
+    string folderPath = "Data/Birds/Birds.json";
+    var jsonStr = File.ReadAllText(folderPath);
+
+    var reult = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr);
+
+    var item = reult.Tbl_Bird.FirstOrDefault(x => x.Id == id);
+
+    if ( item is null)
+    {
+        return Results.BadRequest("No data found");
+    }
+    return Results.Ok(item);
+})
+.WithName("GetBird")
+.WithOpenApi();
+
+
+app.Run();
+
+
+public class BirdResponseModel
+{
+    public BirdModel[] Tbl_Bird { get; set; }
+}
+
+public class BirdModel
+{
+    public int Id { get; set; }
+    public string BirdMyanmarName { get; set; }
+    public string BirdEnglishName { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+}
